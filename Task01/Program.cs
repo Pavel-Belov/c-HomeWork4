087a805cc48d29dc71b0c7378d96a94a8bc4485f
Pavel Belov <job.pb@yandex.ru>
2023-01-24 23:25:39 +0300

    fix: optimize code

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,9 +1,21 @@
 ﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
 
-int[] array = new int[8];
+int[] CreateArray(int size, int min, int max)
+{
+    int[] array = new int[size];
 
-for (int i = 0; i < array.Length; i++)
-    array[i] = new Random().Next(0, 2);
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(min, max + 1);
 
-foreach (int element in array)
-    Console.Write(element + " ");
+    return array;    
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+        Console.Write(element + " ");
+}
+
+int[] array = CreateArray(8, 0, 1);
+Console.WriteLine("Массив случайных чисел от 0 до 1: ");
+PrintArray(array);
